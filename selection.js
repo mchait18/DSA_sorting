d25087b56ead30884ffb544d780318046fd03f41
mchait <miriamchait@gmail.com>
2024-03-31 10:32:31 -0400

    adding selection.js

--- selection.js
+++ selection.js
@@ -1,3 +1,28 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    let i = 0
+    let j = 1
+    while (i < arr.length) {
+        let min = arr[i]
+        let indx = i
+        let indx2 = i
+        while (j < arr.length) {
+            if (arr[j] < min) {
+                min = arr[j]
+                indx2 = j
+                j++
+            }
+            else
+                j++
+        }
+        if (indx !== indx2) {
+            let temp = arr[indx]
+            arr[indx] = arr[indx2]
+            arr[indx2] = temp
+        }
+        i++
+        j = i + 1
+    }
+    return arr
+}
 
 module.exports = selectionSort;
